9ee44cee4ae0d2f7aa34b3f7cd55669a8f8d2d70
Todd Kelly <donkeyskates21@gmail.com>
2021-06-23 15:16:20 -0400

    update the banner date

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-var today = moment();
-$("#currentDay").text(today.format('LLLL'));// used the moment js to create the current day,date and time
+// Time and date 
+var timeDisplayEl = $('#time-display');
 
-var present_hour = today.format("H");//created a variable for storing the hour as an integer value
-console.log(present_hour);
 
-$(".saveBtn").on("click", function() {// Event listener for creating the function to get the schedule time and date
+//display the current date and time in the header 
+function displayTime() {
+  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+  //calls the displaytime function and shows every second on the screen 
+  setInterval(displayTime, 1000);
+
+//connect to the saveBtn  
+$(".saveBtn").on("click", function() {
     var schedule_data = $(this).siblings(".schedule").val();
     var schedule_time = $(this).parent().attr("id");
-    localStorage.setItem(schedule_time, schedule_data );//storing the data in local storage
+    localStorage.setItem(schedule_time, schedule_data );
 });
 
-// uploading the saved data from localStorage 
+//time ID selector
 $("#7 .schedule").val(localStorage.getItem("7"));
 $("#8 .schedule").val(localStorage.getItem("8"));
 $("#9 .schedule").val(localStorage.getItem("9"));
@@ -24,14 +32,14 @@ $("#16 .schedule").val(localStorage.getItem("16"));
 $("#17 .schedule").val(localStorage.getItem("17"));
 $("#18 .schedule").val(localStorage.getItem("18"));
 
-function time_change() {//created a function to compared the time present on the schedule and the present time
+function time_change() {
 
   
     $(".time-block").each(function() {
-        var compare_hour = parseInt($(this).attr("id"));//converted the time already present into an integer
+        var compare_hour = parseInt($(this).attr("id"));
         console.log(compare_hour);
         console.log(compare_hour < present_hour);
-        if (compare_hour < present_hour) {// using the conditions in order to allot the past present and future classes 
+        if (compare_hour < present_hour) {
             $(this).addClass("past").removeClass("present").removeClass("future");
         } 
         else if (compare_hour == present_hour) {
@@ -43,4 +51,4 @@ function time_change() {//created a function to compared the time present on the
     });
   }
   
-  time_change();// call the function created
+  time_change();
